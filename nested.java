fdbc5f6c3925c7575c5c168951906edb82421fb4
ufdilla <learnmore.cumi@gmail.com>
2017-12-08 10:40:07 +0700

    belajar membuat record

---
+++ nested.java
@@ -0,0 +1,26 @@
+/**
+ * nested
+ */
+public class nested 
+{
+
+    private String nama;
+    private int usia;
+    
+    public nested(String nama, int usia) 
+    {
+        this.nama = nama;
+        this.usia = usia;
+    }
+
+    public String getNama() 
+    {
+        return nama;
+    }
+
+    public int getUsia() 
+    {
+        return usia;
+    }
+
+}
